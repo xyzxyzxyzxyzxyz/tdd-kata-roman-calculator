094acf718bfac8723244cc98b895150fde5733fa
XYZ <idm.caz@mailinator.com>
2017-03-26 13:20:08 +0200

    New test: Units must be converted properly Implementation

--- src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberParser.kt
+++ src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberParser.kt
@@ -3,7 +3,22 @@ package com.tdd.katas.romancalculator
 class RomanNumberParser {
 
     fun parseRomanNumber(romanNumber : String) : Int {
-        return -1
+        var value : Int
+
+        when (romanNumber) {
+            "I"     -> value = 1
+            "II"    -> value = 2
+            "III"   -> value = 3
+            "IV"    -> value = 4
+            "V"     -> value = 5
+            "VI"    -> value = 6
+            "VII"   -> value = 7
+            "VIII"  -> value = 8
+            "IX"    -> value = 9
+            else    -> throw IllegalArgumentException("Not a valid unit number")
+        }
+
+        return value
     }
 
 }
