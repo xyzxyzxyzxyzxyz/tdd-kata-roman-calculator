53c0f2f37fd270df12dc449f61ffefc851ad0f35
XYZ <idm.caz@mailinator.com>
2017-03-26 12:14:08 +0200

    New test: 1 + 1 = 2 Refactoring with real, trivial implementation...

--- src/main/kotlin/com/tdd/katas/romancalculator/NumberCalculator.kt
+++ src/main/kotlin/com/tdd/katas/romancalculator/NumberCalculator.kt
@@ -3,7 +3,7 @@ package com.tdd.katas.romancalculator
 class NumberCalculator {
 
     fun sumNumbers(number1: Int, number2: Int) : Int {
-        return 2
+        return number1 + number2
     }
 
 }
