fefd70fe69341cc110e850c284f83f101991e19c
XYZ <idm.caz@mailinator.com>
2017-03-26 19:01:25 +0200

    RomanNumberFormatter New test: single units are formatted properly Failing test

--- src/test/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatterTest.kt
+++ src/test/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatterTest.kt
@@ -1,4 +1,24 @@
 package com.tdd.katas.romancalculator
 
+import org.junit.Test
+import org.junit.Assert.*
+
+
 class RomanNumberFormatterTest {
+
+    @Test
+    fun singleUnitsAreFormattedProperly(){
+        var numberFormatter = RomanNumberFormatter()
+
+        assertEquals("I"    , numberFormatter.formatRomanNumber(1))
+        assertEquals("II"   , numberFormatter.formatRomanNumber(2))
+        assertEquals("III"  , numberFormatter.formatRomanNumber(3))
+        assertEquals("IV"   , numberFormatter.formatRomanNumber(4))
+        assertEquals("V"    , numberFormatter.formatRomanNumber(5))
+        assertEquals("VI"   , numberFormatter.formatRomanNumber(6))
+        assertEquals("VII"  , numberFormatter.formatRomanNumber(7))
+        assertEquals("VIII" , numberFormatter.formatRomanNumber(8))
+        assertEquals("IX"   , numberFormatter.formatRomanNumber(9))
+
+    }
 }
