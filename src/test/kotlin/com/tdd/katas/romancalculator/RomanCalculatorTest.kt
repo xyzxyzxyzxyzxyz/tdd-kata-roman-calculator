998720d6973489cf10f4519593d505b9c14d0822
XYZ <idm.caz@mailinator.com>
2017-03-26 20:56:57 +0200

    RomanCalculator Failing test

--- src/test/kotlin/com/tdd/katas/romancalculator/RomanCalculatorTest.kt
+++ src/test/kotlin/com/tdd/katas/romancalculator/RomanCalculatorTest.kt
@@ -1,9 +1,40 @@
 package com.tdd.katas.romancalculator
 
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
 class RomanCalculatorTest {
 
-    /*
+    var calculator: RomanCalculator? = null
 
-     */
+    @Before
+    fun setUp() {
+        calculator = RomanCalculator()
+    }
+
+
+    @Test
+    fun test() {
+        // 1 + 1 = 2
+        assertSummedRomanNumbersAfterFormattingEquals("I", "I", "II")
+        // 10 + 8 = 18
+        assertSummedRomanNumbersAfterFormattingEquals("X", "VIII", "XVIII")
+        // 100 + 5 = 105
+        assertSummedRomanNumbersAfterFormattingEquals("C", "V", "CV")
+        // 5 + 1012 = 1017
+        assertSummedRomanNumbersAfterFormattingEquals("V", "MXII", "MXVII")
+        // 3578 + 7999 = 11577
+        assertSummedRomanNumbersAfterFormattingEquals("MMMDLXXVIII", "MMMMMMMMCMXCIX", "MMMMMMMMMMMDLXXVII")
+    }
+
+
+    fun assertSummedRomanNumbersAfterFormattingEquals(
+            romanNumber1: String,
+            romanNumber2: String,
+            expectedFormattedResult: String) {
+
+        assertEquals(expectedFormattedResult, calculator!!.addRomanNumbers(romanNumber1, romanNumber2))
+    }
 
 }
