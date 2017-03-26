3b55848da970d29a2abc7b96ba3f28eb6f47e3a2
XYZ <idm.caz@mailinator.com>
2017-03-26 19:07:21 +0200

    RomanNumberFormatter New test: single units are formatted properly Implementation

--- src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatter.kt
+++ src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatter.kt
@@ -2,8 +2,19 @@ package com.tdd.katas.romancalculator
 
 class RomanNumberFormatter {
 
-    fun formatRomanNumber(number: Int) : String {
-        return ""
-    }
+    fun formatRomanNumber(number: Int) : String =
 
+        when(number){
+            0 -> ""
+            1 -> "I"
+            2 -> "II"
+            3 -> "III"
+            4 -> "IV"
+            5 -> "V"
+            6 -> "VI"
+            7 -> "VII"
+            8 -> "VIII"
+            9 -> "IX"
+            else -> throw IllegalArgumentException("Not a unit: " + number)
+        }
 }
