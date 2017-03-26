18aa181aff7b35ee1d717b986428f8824ae05476
XYZ <idm.caz@mailinator.com>
2017-03-26 13:10:38 +0200

    New test: Units must be converted properly Failing

--- src/test/kotlin/com/tdd/katas/romancalculator/RomanNumberParserTest.kt
+++ src/test/kotlin/com/tdd/katas/romancalculator/RomanNumberParserTest.kt
@@ -1,4 +1,23 @@
 package com.tdd.katas.romancalculator
 
+import org.junit.Test
+import org.junit.Assert.*
+
 class RomanNumberParserTest {
+
+    @Test
+    fun unitsAreConvertedProperly(){
+
+        val parser = RomanNumberParser()
+
+        assertEquals(1, parser.parseRomanNumber("I"))
+        assertEquals(2, parser.parseRomanNumber("II"))
+        assertEquals(3, parser.parseRomanNumber("III"))
+        assertEquals(4, parser.parseRomanNumber("IV"))
+        assertEquals(5, parser.parseRomanNumber("V"))
+        assertEquals(6, parser.parseRomanNumber("VI"))
+        assertEquals(7, parser.parseRomanNumber("VII"))
+        assertEquals(8, parser.parseRomanNumber("VIII"))
+        assertEquals(9, parser.parseRomanNumber("IX"))
+    }
 }
