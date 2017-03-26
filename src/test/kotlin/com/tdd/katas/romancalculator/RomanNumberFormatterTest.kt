00371543345dad03d135616298236c8c2c9a3580
XYZ <idm.caz@mailinator.com>
2017-03-26 19:10:14 +0200

    RomanNumberFormatter New test: Tenths numbers are formatted properly Failing test

--- src/test/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatterTest.kt
+++ src/test/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatterTest.kt
@@ -21,4 +21,21 @@ class RomanNumberFormatterTest {
         assertEquals("IX"   , numberFormatter.formatRomanNumber(9))
 
     }
+
+    @Test
+    fun tenthsNumbersAreFormatedProperly() {
+        var numberFormatter = RomanNumberFormatter()
+
+        assertEquals("X"    , numberFormatter.formatRomanNumber(10))
+        assertEquals("XX"   , numberFormatter.formatRomanNumber(20))
+        assertEquals("XXX"  , numberFormatter.formatRomanNumber(30))
+        assertEquals("XL"   , numberFormatter.formatRomanNumber(40))
+        assertEquals("L"    , numberFormatter.formatRomanNumber(50))
+        assertEquals("LX"   , numberFormatter.formatRomanNumber(60))
+        assertEquals("LXX"  , numberFormatter.formatRomanNumber(70))
+        assertEquals("LXXX" , numberFormatter.formatRomanNumber(80))
+        assertEquals("XC"   , numberFormatter.formatRomanNumber(90))
+
+    }
+
 }
