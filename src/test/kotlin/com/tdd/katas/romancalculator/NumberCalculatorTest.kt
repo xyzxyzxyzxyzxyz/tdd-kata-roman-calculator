90041ca67888ce465668c1f756b552642bcaed14
XYZ <idm.caz@mailinator.com>
2017-03-26 12:11:30 +0200

    New test: 1 + 1 = 2 Failing

--- src/test/kotlin/com/tdd/katas/romancalculator/NumberCalculatorTest.kt
+++ src/test/kotlin/com/tdd/katas/romancalculator/NumberCalculatorTest.kt
@@ -1,4 +1,20 @@
 package com.tdd.katas.romancalculator
 
+import org.junit.Test
+import org.junit.Assert.*
+
 class NumberCalculatorTest {
+
+    @Test
+    fun givenOneAndOneMustSumTwo() {
+        val number1 = 1
+        val number2 = 1
+
+        val calc : NumberCalculator = NumberCalculator()
+
+        val result = calc.sumNumbers(number1, number2)
+
+        assertEquals("Should be two", 2, result)
+    }
+
 }
