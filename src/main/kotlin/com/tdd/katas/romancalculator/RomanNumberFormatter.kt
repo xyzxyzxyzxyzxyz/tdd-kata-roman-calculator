efdcf0fdae602b67ab7d01ff31e2e7e31792f31f
XYZ <idm.caz@mailinator.com>
2017-03-26 20:19:39 +0200

    RomanNumberFormatter New test: Composite numbers are formatted properly Implementation + refactoring

--- src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatter.kt
+++ src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberFormatter.kt
@@ -4,24 +4,30 @@ class RomanNumberFormatter {
 
     fun formatRomanNumber(number: Int): String {
 
-        if (number < 10) {
-            return formatUnits(number)
-        }
+        var number = number
 
-        if (number < 100) {
-            return formatTenths(number)
-        }
+        val formattedNumber = StringBuilder()
 
-        if (number < 1000) {
-            return formatHundreds(number)
-        }
+        val numberOfThousands = number / 1000
+        formattedNumber.append(formatThousands(numberOfThousands))
 
-        return formatThousands(number)
+        number %= 1000
+        val numberOfHundreds = number / 100
+        formattedNumber.append(formatHundreds(numberOfHundreds))
+
+        number %= 100
+        val numberOfTenths = number / 10
+        formattedNumber.append(formatTenths(numberOfTenths))
+
+        number %= 10
+        formattedNumber.append(formatUnits(number))
+
+        return formattedNumber.toString()
     }
 
 
-    private fun formatUnits(number: Int) : String =
-        when(number) {
+    private fun formatUnits(numberOfUnits: Int) : String =
+        when(numberOfUnits) {
             0 -> ""
             1 -> "I"
             2 -> "II"
@@ -32,40 +38,40 @@ class RomanNumberFormatter {
             7 -> "VII"
             8 -> "VIII"
             9 -> "IX"
-            else -> throw IllegalArgumentException("Not a unit: " + number)
+            else -> throw IllegalArgumentException("Not a unit: " + numberOfUnits)
         }
 
-    private fun formatTenths(number: Int) : String =
-        when(number) {
-            0  -> ""
-            10 -> "X"
-            20 -> "XX"
-            30 -> "XXX"
-            40 -> "XL"
-            50 -> "L"
-            60 -> "LX"
-            70 -> "LXX"
-            80 -> "LXXX"
-            90 -> "XC"
-            else -> throw IllegalArgumentException("Not a tenth: " + number)
+    private fun formatTenths(numberOfTenths: Int) : String =
+        when(numberOfTenths) {
+            0 -> ""
+            1 -> "X"
+            2 -> "XX"
+            3 -> "XXX"
+            4 -> "XL"
+            5 -> "L"
+            6 -> "LX"
+            7 -> "LXX"
+            8 -> "LXXX"
+            9 -> "XC"
+            else -> throw IllegalArgumentException("Not a tenth: " + numberOfTenths)
         }
 
-    private fun formatHundreds(number: Int) : String =
-            when(number) {
-                100 -> "C"
-                200 -> "CC"
-                300 -> "CCC"
-                400 -> "CD"
-                500 -> "D"
-                600 -> "DC"
-                700 -> "DCC"
-                800 -> "DCCC"
-                900 -> "CM"
-                else -> throw IllegalArgumentException("Not a tenth: " + number)
+    private fun formatHundreds(numberOfHundreds: Int) : String =
+            when(numberOfHundreds) {
+                0 -> ""
+                1 -> "C"
+                2 -> "CC"
+                3 -> "CCC"
+                4 -> "CD"
+                5 -> "D"
+                6 -> "DC"
+                7 -> "DCC"
+                8 -> "DCCC"
+                9 -> "CM"
+                else -> throw IllegalArgumentException("Not a hundred: " + numberOfHundreds)
             }
 
-    private fun  formatThousands(number: Int): String {
-        val numberOfThousands = number / 1000
+    private fun formatThousands(numberOfThousands: Int) : String {
         return "M".repeat(numberOfThousands)
     }
 
