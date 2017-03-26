978189047f7dce0c9c208c5cdb9893c43a7c8129
XYZ <idm.caz@mailinator.com>
2017-03-26 21:03:30 +0200

    RomanCalculator Implementation

--- src/main/kotlin/com/tdd/katas/romancalculator/RomanCalculator.kt
+++ src/main/kotlin/com/tdd/katas/romancalculator/RomanCalculator.kt
@@ -3,7 +3,16 @@ package com.tdd.katas.romancalculator
 class RomanCalculator {
 
     fun addRomanNumbers(romanNumber1 : String, romanNumber2 : String) : String {
-        return ""
+        var parser = RomanNumberParser()
+        var calculator = NumberCalculator()
+        var formatter = RomanNumberFormatter()
+
+        val romanNumber1Parsed = parser.parseRomanNumber(romanNumber1)
+        val romanNumber2Parsed = parser.parseRomanNumber(romanNumber2)
+
+        val sum = calculator.sumNumbers(romanNumber1Parsed, romanNumber2Parsed)
+
+        return formatter.formatRomanNumber(sum)
     }
 
 }
