6fe88289055c25bb14a82e7265d83b31b357210e
XYZ <idm.caz@mailinator.com>
2017-03-26 13:35:44 +0200

    New test: Single letter numbers are properly converted Implementation

--- src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberParser.kt
+++ src/main/kotlin/com/tdd/katas/romancalculator/RomanNumberParser.kt
@@ -3,7 +3,7 @@ package com.tdd.katas.romancalculator
 class RomanNumberParser {
 
     fun parseRomanNumber(romanNumber : String) : Int {
-        var value : Int
+        var value : Int = -1
 
         when (romanNumber) {
             "I"     -> value = 1
@@ -15,10 +15,27 @@ class RomanNumberParser {
             "VII"   -> value = 7
             "VIII"  -> value = 8
             "IX"    -> value = 9
-            else    -> throw IllegalArgumentException("Not a valid unit number")
         }
 
-        return value
+        if (value != -1) {
+            return value
+        }
+
+        when (romanNumber) {
+            "X"     -> value = 10
+            "L"     -> value = 50
+            "C"     -> value = 100
+            "D"     -> value = 500
+            "M"     -> value = 1000
+        }
+
+        if (value != -1) {
+            return value
+        }
+
+        throw IllegalArgumentException("Not a valid unit number: " + romanNumber)
     }
+
+
 
 }
